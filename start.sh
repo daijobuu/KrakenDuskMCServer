c425251348bfca1b81b820bf9f8800ac99c70086
Ubuntu <azureuser@myVM.pkoxmvkwuyvuln12hths2jvceb.bx.internal.cloudapp.net>
2022-01-12 07:23:27 +0000

    commiiting for latest.log

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+JAR=server.jar
+MAXRAM=3072M
+MINRAM=1024M
+TIME=20
+
+
+while [ true ]; do
+    sudo java -Xmx$MAXRAM -Xms$MINRAM -jar $JAR nogui
+    if [[ ! -d "exit_codes" ]]; then
+        mkdir "exit_codes";
+    fi
+    if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
+        touch "exit_codes/server_exit_codes.log";
+    fi
+    echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
+    echo "----- Press enter to prevent the server from restarting in $TIME seconds -----";
+    read -t $TIME input;
+    if [ $? == 0 ]; then
+        break;
+    else
+        echo "------------------- SERVER RESTARTS -------------------";
+    fi
+done
